99f3f3394af03d5bbb71e5e3a1a8bced29cf718b
abutalha08 <taufique752@gmail.com>
2026-04-29 01:38:45 +0600

    create custom not-found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+
+
+const NotFound = () => {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h2>Page Not Found</h2>
+            <p>The page you are looking for does not exist.</p>
+        </div>
+    );
+};
+
+export default NotFound;
