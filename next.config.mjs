45317c1a094d6d012dfdc4ee6cc609aa2a6e2146
abutalha08 <taufique752@gmail.com>
2026-04-29 01:27:40 +0600

    Image Optimization in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+
+  // https://plus.unsplash.com/premium_photo-1677337458939-84d622293793
+
+  
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '**',
+        search: '',
+      },
+      
+      // https://i.ibb.co/Xp3sL2h/1.png
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
